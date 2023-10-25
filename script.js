393cc3dfb0464abe3e837c32cc713737fbd93373
gtsoul-tech <56584633+gtsoul-tech@users.noreply.github.com>
2023-10-25 14:49:53 +0300

    add forecast days,hours

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let url;
 
 async function getWeather(city) {
   try {
-    console.time("API call");
+    //console.time("API call");
     url = `https://api.weatherapi.com/v1/forecast.json?key=${APIkey}&q=${city}&days=${days}`;
     const response = await fetch(url, { mode: "cors" });
     if (response.ok) {
       const weatherData = await response.json();
-      console.timeEnd("API call");
+      //console.timeEnd("API call");
 
       return weatherData;
     } else {
@@ -25,7 +25,7 @@ const form = document.getElementById("cityForm");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   city = Object.fromEntries(new FormData(e.target).entries());
-  render(city.cityName, "metrics");
+  render(city.cityName, "metrics", "daily");
   form.reset();
 });
 
@@ -65,9 +65,9 @@ async function mainInfo(weatherData, measurementSystem) {
     }
     metrics.addEventListener("click", () => {
       if (measurementSystem == "metrics") {
-        render(weatherData.location.name, "ImperialSystem");
+        render(weatherData.location.name, "ImperialSystem", "daily");
       } else {
-        render(weatherData.location.name, "metrics");
+        render(weatherData.location.name, "metrics", "daily");
       }
     });
     temperature.classList.add("temperature");
@@ -109,15 +109,108 @@ async function leftInfo(weatherData, measurementSystem) {
   }
 }
 
-async function forecastInfo(weatherData, measurementSystem) {
-  //2 diaforetika render ena gia daily ena gia hourly
-  //loopa 3 meres to array
-  //poia mera einai
-  //temperature
-  //feelslike mikrotero
-  //kai eikona
+async function forecastDaily(weatherData, measurementSystem) {
+  if (weatherData != false) {
+    const forecastContent = document.querySelector(".forecastContent");
+    forecastContent.querySelectorAll("*").forEach((n) => n.remove());
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    weatherData.forecast.forecastday.forEach((item, index) => {
+      const forecastContent = document.querySelector(".forecastContent");
+
+      forecastContent.classList.remove("flex-start");
+      forecastContent.classList.add("spaced-evenly");
+      const dayBox = document.createElement("div");
+      dayBox.classList.add("dayBox");
+      const day = document.createElement("div");
+      const date = new Date(item.date_epoch * 1000);
+      day.textContent = days[date.getDay()];
+      day.classList.add("dayName");
+      const temperatureMax = document.createElement("div");
+      const temperatureMin = document.createElement("div");
+      temperatureMin.classList.add("smaller");
+      const img = document.createElement("img");
+      img.classList.add("square");
+
+      img.src = "https:" + item.day.condition.icon;
+      if (measurementSystem == "metrics") {
+        temperatureMax.textContent = "Max " + item.day.maxtemp_c + " °C";
+        temperatureMin.textContent = "Min " + item.day.mintemp_c + " °C";
+      } else {
+        temperatureMax.textContent = "Max " + item.day.maxtemp_f + " °F";
+        temperatureMin.textContent = "Min " + item.day.mintemp_f + " °F";
+      }
+      dayBox.appendChild(day);
+      dayBox.appendChild(temperatureMax);
+      dayBox.appendChild(temperatureMin);
+      dayBox.appendChild(img);
+      forecastContent.appendChild(dayBox);
+    });
+  }
 }
-async function render(city, measurementSystem) {
+
+async function forecastHourly(weatherData, measurementSystem) {
+  if (weatherData != false) {
+    const forecastContent = document.querySelector(".forecastContent");
+    forecastContent.classList.add("flex-start");
+    forecastContent.classList.remove("spaced-evenly");
+    forecastContent.querySelectorAll("*").forEach((n) => n.remove());
+    weatherData.forecast.forecastday.forEach((item, index) => {
+      const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+      item.hour.forEach((hours, index) => {
+        const hourBox = document.createElement("div");
+        hourBox.classList.add("dayBox");
+        const day = document.createElement("div");
+        const dateDay = new Date(item.date_epoch * 1000);
+        day.textContent = days[dateDay.getDay()];
+        day.classList.add("smaller");
+        const hour = document.createElement("div");
+        const date = new Date(hours.time_epoch * 1000);
+        hour.textContent = date.toLocaleString("en-US", {
+          hour: "numeric",
+          hour12: true,
+        });
+        hour.classList.add("dayName");
+
+        const temperature = document.createElement("div");
+
+        const img = document.createElement("img");
+        img.classList.add("square");
+
+        img.src = "https:" + hours.condition.icon;
+
+        if (measurementSystem == "metrics") {
+          temperature.textContent = hours.temp_c + " °C";
+        } else {
+          temperature.textContent = hours.temp_f + " °F";
+        }
+        hourBox.appendChild(day);
+        hourBox.appendChild(hour);
+        hourBox.appendChild(temperature);
+        hourBox.appendChild(img);
+
+        forecastContent.appendChild(hourBox);
+      });
+    });
+  }
+}
+async function render(city, measurementSystem, which) {
   let weatherData;
   try {
     weatherData = await getWeather(city);
@@ -127,6 +220,36 @@ async function render(city, measurementSystem) {
   console.log(weatherData);
   mainInfo(weatherData, measurementSystem);
   leftInfo(weatherData, measurementSystem);
-  forecastInfo(weatherData, measurementSystem);
+  if (which == "daily") {
+    forecastDaily(weatherData, measurementSystem);
+  } else if (which == "hourly") {
+    forecastHourly(weatherData, measurementSystem);
+  }
 }
-render(city, "metrics");
+render(city, "metrics", "daily");
+
+const dailyButton = document.querySelector(".dailyButton");
+dailyButton.addEventListener("click", (city) => {
+  const cityName = document.querySelector(".name");
+  const measurementSystem = document.querySelector(".temperature");
+  let mm;
+  if (measurementSystem.textContent.includes("C")) {
+    mm = "metrics";
+  } else {
+    mm = "ImperialSystem";
+  }
+  render(cityName.textContent, mm, "daily");
+});
+
+const hourlyButton = document.querySelector(".hourlyButton");
+hourlyButton.addEventListener("click", () => {
+  const cityName = document.querySelector(".name");
+  const measurementSystem = document.querySelector(".temperature");
+  let mm;
+  if (measurementSystem.textContent.includes("C")) {
+    mm = "metrics";
+  } else {
+    mm = "ImperialSystem";
+  }
+  render(cityName.textContent, mm, "hourly");
+});
